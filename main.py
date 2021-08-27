cf1b54d58aa55f9fc47b9178733cc1350ac029ba
LikeableJuniper <83668921+LikeableJuniper@users.noreply.github.com>
2021-08-27 10:38:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from PIL import ImageTk, Image
 from functools import partial
 import socket, json, threading, logging
 
+
+
 root = tk.Tk()
 root.attributes("-fullscreen", True)
 deck = []
@@ -10,66 +12,69 @@ handCardSlots = []
 opponentCardSlots = []
 userCardSlots = []
 handcards = []
+imgKlee = None
 
 #! Networking variables
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 65432
 sendData = None
 recvData = None
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientsocket = None
 canStart = False
 
 
-def clientListener():
-    global HOST, PORT, recvData, s, canStart
-    s.connect((HOST, PORT))
-    print("Connected")
+
+def serverListener():
+    global HOST, PORT, recvData, clientsocket, canStart
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(1)
+    clientsocket, addr = s.accept()
     canStart = True
     while True:
-        recvData = s.recv(1024)
-        print(recvData)
+        recvData = clientsocket.recv(1024)
+        clientsocket, addr = s.accept()
 
 
-def clientSender():
-    global sendData, s
+def serverSender():
+    global sendData, clientsocket, canStart
     while True:
         if sendData is not None:
-            s.sendall(sendData)
+            clientsocket.sendall(sendData)
             sendData = None
 
 
 def keepUpdated():
-    global recvData, opponentCardSlots
+    global recvData, opponentCardSlots, imgKlee
     while True:
         if recvData is not None:
-            print("Received Data {0}".format(recvData))
+            print(recvData)
             data = json.loads(recvData)
+            recvData = None
             opponentCardSlots[data["index"]]["jsonindex"] = data["jsonindex"]
             opponentCardSlots[data["index"]]["hasImage"] = True
-            try:
-                opponentCardSlots[data["index"]]["Button"].config(image=ImageTk.PhotoImage(Image.open(json.load(open("characters.json", "r"))[data["jsonindex"]]["file"]).resize([177, 100])))
-                opponentCardSlots[data["index"]]["Button"].pack()
-                print("Finished")
-            except Exception as e:
-                print(e)
-            finally:
-                recvData = None
+            print(json.load(open("characters.json", "r"))[data["jsonindex"]]["file"])
+            img = ImageTk.PhotoImage(Image.open(json.load(open("characters.json", "r"))[data["jsonindex"]]["file"]))
+            opponentCardSlots[data["index"]]["Button"].config(image=imgKlee)
 
 
-threading.Thread(target=keepUpdated).start()
-threading.Thread(target=clientListener).start()
-threading.Thread(target=clientSender).start()
 
 
-imgKlee = Image.open("Images/klee.png")
-imgKlee2=imgKlee.resize([177, 100])
-imgKlle = ImageTk.PhotoImage(imgKlee2)
+t1 = threading.Thread(target=keepUpdated)
+t1.setDaemon(True)
+t1.start()
+t2 = threading.Thread(target=serverListener)
+t2.setDaemon(True)
+t2.start()
+t3 = threading.Thread(target=serverSender)
+t3.setDaemon(True)
+t3.start()
 
-imgT = Image.open("Images/Traveler.png")
-imgT2 = imgT.resize([110, 110])
-imgT3 = ImageTk.PhotoImage(imgT2)
+imgKlee = ImageTk.PhotoImage(Image.open("Images/klee.png").resize([100, 177]))
 
-selected = 0
+imgT3 = ImageTk.PhotoImage(Image.open("Images/Traveler.png").resize([110, 110]))
+
+selected = 0 
 
 def handButtonClick(n):
     global selected
@@ -87,6 +92,7 @@ def buttonClick(n, ownedByUser):
             userCardSlots[n]["hasImage"] = True
             toSend = '{"index": ' + str(n) + ', "jsonindex": '+ str(handCardSlots[selected]["jsonindex"]) + '}'
             sendData = bytes(toSend.encode("utf-8"))
+            
 
 
 def exitWindow():
@@ -95,21 +101,21 @@ def exitWindow():
     toplevel.iconbitmap("Images/toplevelIcon.ico")
     toplevel.geometry("160x90")
     tk.Label(toplevel, text="Do you really want to exit?").place(x=10, y=-20, height=90, width=150)
-    tk.Button(toplevel, text="Ok", bg="red", command=exit).place(x=10, y=50, height=30, width=60)
+    tk.Button(toplevel, text="Ok", bg="red", command=root.destroy).place(x=10, y=50, height=30, width=60)
     tk.Button(toplevel, text="Cancel", bg="green", command=toplevel.destroy).place(x=90, y=50, height=30, width=60)
 
 
 tk.Button(text="Exit", bg="red", command=exitWindow).place(x=10, y=10, height=30, width=50)
 
-for i in range(5):
-    opponentCardSlots.append({"Button": tk.Button(root, command=partial(buttonClick, i, ownedByUser=False)), "hasImage": False, "effects": []})
 
 for i in range(5):
-    userCardSlots.append({"Button": tk.Button(root, command=partial(buttonClick, i, ownedByUser=True)), "hasImage": False, "effects": []})
-
+    opponentCardSlots.append({"Button": tk.Button(root, command=partial(buttonClick, i, ownedByUser=False), text=i), "hasImage": False, "effects": [], "jsonindex": None})
 
 for i in range(5):
-    handCardSlots.append({"Button": tk.Button(root, command=partial(handButtonClick, i)), "hasImage": False})
+    userCardSlots.append({"Button": tk.Button(root, command=partial(buttonClick, i, ownedByUser=True), text=i), "hasImage": False, "effects": [], "jsonindex": None})
+
+for i in range(5):
+    handCardSlots.append({"Button": tk.Button(root, command=partial(handButtonClick, i)), "hasImage": False, "jsonindex": None})
 
 
 opponentCardSlots[0]["Button"].place(x=825, y=190, height=170, width=100)
@@ -124,9 +130,8 @@ userCardSlots[3]["Button"].place(x=600, y=680, height=170, width=100)
 userCardSlots[4]["Button"].place(x=750, y=680, height=170, width=100)
 handCardSlots[0]["Button"].place(x=1200, y=500, height=170, width=100)
 handCardSlots[1]["Button"].place(x=1300, y=500, height=170, width=100)
-handCardSlots[0]["Button"].config(image=imgKlle); handCardSlots[0]["jsonindex"] = 0; handCardSlots[0]["hasImage"] = True
+handCardSlots[0]["Button"].config(image=imgKlee); handCardSlots[0]["jsonindex"] = 0; handCardSlots[0]["hasImage"] = True
 handCardSlots[1]["Button"].config(image=imgT3); handCardSlots[1]["jsonindex"] = 1; handCardSlots[1]["hasImage"] = True
-
 
 while True:
     if canStart:
